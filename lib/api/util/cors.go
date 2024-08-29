09441864cdf01b5d5901bfb57d8c910f54af6ce2
Ingo <roessner@infai.org>
2024-08-29 11:19:55 +0200

    add 'X-Total-Count' header to cors 'Access-Control-Allow-Headers'

--- lib/api/util/cors.go
+++ lib/api/util/cors.go
@@ -32,7 +32,7 @@ func (this *CorsMiddleware) ServeHTTP(res http.ResponseWriter, req *http.Request
 		origin = "*"
 	}
 	res.Header().Set("Access-Control-Allow-Origin", origin)
-	res.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization, Authorization")
+	res.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization, Authorization, X-Total-Count")
 	res.Header().Set("Access-Control-Allow-Credentials", "true")
 	res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
