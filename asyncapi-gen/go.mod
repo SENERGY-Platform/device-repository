4bed0c795f365c275b473d01bfabf56d49588e80
Ingo <roessner@infai.org>
2025-12-16 14:55:45 +0100

    kafka docker testcontainer fix

--- asyncapi-gen/go.mod
+++ asyncapi-gen/go.mod
@@ -1,6 +1,6 @@
 module asyncapi-gen
 
-go 1.24.0
+go 1.25
 
 require (
 	github.com/SENERGY-Platform/device-repository v0.0.0-00010101000000-000000000000
@@ -8,11 +8,11 @@ require (
 )
 
 require (
-	github.com/SENERGY-Platform/models/go v0.0.0-20241007061544-de7132ae94e4 // indirect
+	github.com/SENERGY-Platform/models/go v0.0.0-20251202070403-e7e5579f7111 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/pierrec/lz4/v4 v4.1.21 // indirect
-	github.com/segmentio/kafka-go v0.4.47 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/segmentio/kafka-go v0.4.49 // indirect
 	github.com/swaggest/jsonschema-go v0.3.39 // indirect
 	github.com/swaggest/refl v1.1.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- lib/tests/docker/kafka.go
+++ lib/tests/docker/kafka.go
@@ -18,14 +18,74 @@ package docker
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math"
+	"strconv"
 	"sync"
 
+	"github.com/docker/go-connections/nat"
+	"github.com/testcontainers/testcontainers-go"
 	kafka_test "github.com/testcontainers/testcontainers-go/modules/kafka"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func Kafka(ctx context.Context, wg *sync.WaitGroup) (kafkaUrl string, err error) {
-	c, err := kafka_test.Run(ctx, "confluentinc/confluent-local:7.5.0")
+	kafkaport, err := GetFreePort()
+	if err != nil {
+		return kafkaUrl, err
+	}
+	provider, err := testcontainers.NewDockerProvider(testcontainers.DefaultNetwork("bridge"))
+	if err != nil {
+		return kafkaUrl, err
+	}
+	hostIp, err := provider.GetGatewayIP(ctx)
+	if err != nil {
+		return kafkaUrl, err
+	}
+	kafkaUrl = hostIp + ":" + strconv.Itoa(kafkaport)
+	log.Println("host ip: ", hostIp)
+	log.Println("host port: ", kafkaport)
+	log.Println("kafkaUrl url: ", kafkaUrl)
+
+	c, err := kafka_test.Run(ctx, "confluentinc/confluent-local:7.5.0",
+		testcontainers.WithExposedPorts(strconv.Itoa(kafkaport)+":9093/tcp"),
+		testcontainers.WithEnv(
+			map[string]string{
+				"KAFKA_LISTENERS":                                "PLAINTEXT://0.0.0.0:9093,BROKER://0.0.0.0:9092,CONTROLLER://0.0.0.0:9094",
+				"KAFKA_REST_BOOTSTRAP_SERVERS":                   "PLAINTEXT://0.0.0.0:9093,BROKER://0.0.0.0:9092,CONTROLLER://0.0.0.0:9094",
+				"KAFKA_LISTENER_SECURITY_PROTOCOL_MAP":           "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT,CONTROLLER:PLAINTEXT",
+				"KAFKA_INTER_BROKER_LISTENER_NAME":               "BROKER",
+				"KAFKA_BROKER_ID":                                "1",
+				"KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR":         "1",
+				"KAFKA_OFFSETS_TOPIC_NUM_PARTITIONS":             "1",
+				"KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR": "1",
+				"KAFKA_TRANSACTION_STATE_LOG_MIN_ISR":            "1",
+				"KAFKA_LOG_FLUSH_INTERVAL_MESSAGES":              strconv.Itoa(math.MaxInt),
+				"KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS":         "0",
+				"KAFKA_NODE_ID":                                  "1",
+				"KAFKA_PROCESS_ROLES":                            "broker,controller",
+				"KAFKA_CONTROLLER_LISTENER_NAMES":                "CONTROLLER",
+			},
+		),
+		testcontainers.WithLifecycleHooks(testcontainers.ContainerLifecycleHooks{
+			PostStarts: []testcontainers.ContainerHook{
+				func(ctx context.Context, c testcontainers.Container) error {
+					if err := copyStarterScript(ctx, c, kafkaUrl); err != nil {
+						log.Println("ERROR: copy starter script: ", err)
+						return fmt.Errorf("copy starter script: %w", err)
+					}
+
+					err = wait.ForLog(".*Transitioning from RECOVERY to RUNNING.*").AsRegexp().WaitUntilReady(ctx, c)
+					if err != nil {
+						log.Println("ERROR: wait for log: ", err)
+						return err
+					}
+					return nil
+				},
+			},
+		}),
+	)
 	if err != nil {
 		return kafkaUrl, err
 	}
@@ -35,9 +95,40 @@ func Kafka(ctx context.Context, wg *sync.WaitGroup) (kafkaUrl string, err error)
 		<-ctx.Done()
 		log.Println("DEBUG: remove container kafka", c.Terminate(context.Background()))
 	}()
-	brokers, err := c.Brokers(ctx)
-	if err != nil {
-		return kafkaUrl, err
+
+	return kafkaUrl, err
+}
+
+func copyStarterScript(ctx context.Context, c testcontainers.Container, kafkaUrl string) error {
+	const publicPort = nat.Port("9093/tcp")
+	const starterScript = "/usr/sbin/testcontainers_start.sh"
+	// starterScript {
+	const starterScriptContent = `#!/bin/bash
+source /etc/confluent/docker/bash-config
+export KAFKA_ADVERTISED_LISTENERS=%s,BROKER://%s:9092
+echo Starting Kafka KRaft mode
+sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
+echo 'kafka-storage format --ignore-formatted -t "$(kafka-storage random-uuid)" -c /etc/kafka/kafka.properties' >> /etc/confluent/docker/configure
+echo '' > /etc/confluent/docker/ensure
+/etc/confluent/docker/configure
+/etc/confluent/docker/launch`
+
+	if err := wait.ForMappedPort(publicPort).
+		WaitUntilReady(ctx, c); err != nil {
+		return fmt.Errorf("wait for mapped port: %w", err)
 	}
-	return brokers[0], err
+
+	inspect, err := c.Inspect(ctx)
+	if err != nil {
+		return fmt.Errorf("inspect: %w", err)
+	}
+
+	hostname := inspect.Config.Hostname
+
+	scriptContent := fmt.Sprintf(starterScriptContent, "PLAINTEXT://"+kafkaUrl, hostname)
+
+	if err := c.CopyToContainer(ctx, []byte(scriptContent), starterScript, 0o755); err != nil {
+		return fmt.Errorf("copy to container: %w", err)
+	}
+	return nil
 }
